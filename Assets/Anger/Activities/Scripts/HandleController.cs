ce0bb9b9a77c146dad6843929d83fa5b6c8b6866
noran117 <noranmanama9@gmail.com>
2026-02-24 19:34:10 +0200

    editHandleScript

--- Assets/Anger/Activities/Scripts/HandleController.cs
+++ Assets/Anger/Activities/Scripts/HandleController.cs
@@ -1,9 +1,110 @@
 using System.Collections;
+using System.Collections.Generic;
+
 using UnityEngine;
 
 public class HandleController : MonoBehaviour
 {
-    [Header("Group 1")]
+
+    [System.Serializable]
+    public class LeverGroup
+    {
+        public GameObject[] pieces;   // القطع المرتبطة بهذا المقبض
+        public bool correctState;     // الحالة المطلوبة للحل (true = Up)
+        [HideInInspector] public bool currentState;
+        [HideInInspector] public Vector3[] basePositions;
+    }
+
+    public List<LeverGroup> leverGroups = new List<LeverGroup>();
+
+    public float moveHeight = 2f;
+    public float moveSpeed = 3f;
+
+    private bool puzzleSolved = false;
+
+    void Start()
+    {
+        foreach (var group in leverGroups)
+        {
+            group.basePositions = new Vector3[group.pieces.Length];
+
+            for (int i = 0; i < group.pieces.Length; i++)
+            {
+                // نحسب مكان المشي السفلي
+                group.basePositions[i] =
+                    group.pieces[i].transform.position - Vector3.up * moveHeight;
+            }
+        }
+    }
+    public void LeverUp(int index)
+    {
+        if (puzzleSolved) return;
+
+        leverGroups[index].currentState = true;
+        StartCoroutine(MoveGroup(index, true));
+        CheckPuzzle();
+    }
+
+    public void LeverDown(int index)
+    {
+        if (puzzleSolved) return;
+
+        leverGroups[index].currentState = false;
+        StartCoroutine(MoveGroup(index, false));
+        CheckPuzzle();
+    }
+    // يتم استدعاؤها من الـ Lever
+    public void LeverChanged(int index, bool isUp)
+    {
+        if (puzzleSolved) return;
+
+        leverGroups[index].currentState = isUp;
+
+        StartCoroutine(MoveGroup(index, isUp));
+        CheckPuzzle();
+    }
+
+    IEnumerator MoveGroup(int index, bool moveUp)
+    {
+        var group = leverGroups[index];
+
+        float elapsed = 0f;
+        float duration = 0.6f;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+
+            for (int i = 0; i < group.pieces.Length; i++)
+            {
+                Vector3 target = moveUp
+                    ? group.basePositions[i] + Vector3.up * moveHeight
+                    : group.basePositions[i];
+
+                group.pieces[i].transform.position =
+                    Vector3.Lerp(group.pieces[i].transform.position, target, Time.deltaTime * moveSpeed);
+            }
+
+            yield return null;
+        }
+    }
+
+    void CheckPuzzle()
+    {
+        for (int i = 0; i < leverGroups.Count; i++)
+        {
+            if (leverGroups[i].currentState != leverGroups[i].correctState)
+                return;
+        }
+
+        puzzleSolved = true;
+        Debug.Log("Puzzle Solved! Bridge Complete!");
+    }
+
+}
+
+/*
+[Header("Group 1")]
     public GameObject[] group1;
 
     [Header("Group 2")]
@@ -75,5 +176,4 @@ public class HandleController : MonoBehaviour
             yield return null;
         }
     }
-
-}
+    */
