6de6daa7c3493fe367ceea8f176aeab3e2de6617
noran117 <noranmanama9@gmail.com>
2026-03-03 12:07:24 +0200

    Link train animation to movement code

--- Assets/Happiness/Scripts/StartMove.cs
+++ Assets/Happiness/Scripts/StartMove.cs
@@ -2,13 +2,40 @@ using UnityEngine;
 
 public class StartMove : MonoBehaviour
 {
+ public Animator [] animators;
+    private BNG.MoveToWaypoint moveScript;
+    private bool wasMoving = false; 
+    private void Awake()
+    {
+        moveScript = GetComponent<BNG.MoveToWaypoint>();
+    }
+    void Update()
+    {
+        if (moveScript == null|| animators == null) return;
+
+        bool isCurrentlyMoving = moveScript.IsActive && !moveScript.ReachedDestination;
+
+        // إذا الحالة تغيرت فقط
+        if (isCurrentlyMoving != wasMoving)
+        {
+            wasMoving = isCurrentlyMoving;
+            foreach (var animator in animators)
+            {
+                animator.SetBool("isMoving", isCurrentlyMoving);
+            }
+
+            Debug.Log(isCurrentlyMoving ? "moving" : "not moving");
+        }
+    }
+
+
     void OnTriggerEnter(Collider other)
     {
-        //Debug.Log(other.tag);
         if (other.CompareTag("Player"))
         {
             Debug.Log("player entered");
-            GetComponent<BNG.MoveToWaypoint>().IsActive = true;
+            moveScript.IsActive = true;
+           // GetComponent<BNG.MoveToWaypoint>().IsActive = true;
         }
     }
 
