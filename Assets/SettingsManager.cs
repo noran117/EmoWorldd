efa54082d63d6143e17d0465b28c4cff4864b66d
arwajsh21 <arwa.j.sh21@gmail.com>
2026-04-05 13:35:08 +0300

    Main scene UI design

---
+++ Assets/SettingsManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class SettingsManager : MonoBehaviour
+{
+    [Header("Skybox Settings")]
+    public float defaultExposure = 1.0f;
+
+    [Header("Audio Settings")]
+    public AudioSource bgMusicSource; 
+
+    void Start()
+    {
+        //  Õ„Ì· ≈⁄œ«œ«  «·≈÷«¡…
+        float savedExposure = PlayerPrefs.GetFloat("SkyboxExposure", defaultExposure);
+        RenderSettings.skybox.SetFloat("_Exposure", savedExposure);
+
+        //  Õ„Ì· ≈⁄œ«œ«  «·’Ê 
+        float savedVolume = PlayerPrefs.GetFloat("MusicVolume", 0.5f);
+        if (bgMusicSource != null) bgMusicSource.volume = savedVolume;
+    }
+
+    public void SetSkyboxBrightness(float value)
+    {
+        RenderSettings.skybox.SetFloat("_Exposure", value);
+        PlayerPrefs.SetFloat("SkyboxExposure", value);
+    }
+
+    
+    public void SetMusicVolume(float value)
+    {
+        if (bgMusicSource != null)
+        {
+            bgMusicSource.volume = value;
+            PlayerPrefs.SetFloat("MusicVolume", value); 
+        }
+    }
+}
