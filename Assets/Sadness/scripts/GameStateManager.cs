ab1e5ca08f6bf7fa953aa9fb0f6152b97bbf2bc9
noran117 <142379090+noran117@users.noreply.github.com>
2026-04-08 14:58:41 +0300

    Update GameStateManager.cs

--- Assets/Sadness/scripts/GameStateManager.cs
+++ Assets/Sadness/scripts/GameStateManager.cs
@@ -200,7 +200,7 @@ public class GameStateManager : MonoBehaviour
     IEnumerator LoadSceneAfterDelay()
     {
         yield return new WaitForSeconds(20f);
-       // SceneManager.LoadScene("Main_Scene");
+       SceneManager.LoadScene("Main_Scene");
     }
 
 }
